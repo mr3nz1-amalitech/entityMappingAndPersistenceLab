4dd5d995e96d712defa0d6560141677b13b7dbb0
mr3nz1 <paterne.murenzi@amalitech.org>
2024-08-28 20:05:01 +0200

    caching and transactions

--- src/main/java/com/mr3nz1/entityMappingAndPersistenceLab/EntityMappingAndPersistenceLabApplication.java
+++ src/main/java/com/mr3nz1/entityMappingAndPersistenceLab/EntityMappingAndPersistenceLabApplication.java
@@ -3,7 +3,9 @@ package com.mr3nz1.entityMappingAndPersistenceLab;
 import com.mr3nz1.entityMappingAndPersistenceLab.services.EmployeeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class EntityMappingAndPersistenceLabApplication {
     public static void main(String[] args) {
--- src/main/java/com/mr3nz1/entityMappingAndPersistenceLab/services/EmployeeService.java
+++ src/main/java/com/mr3nz1/entityMappingAndPersistenceLab/services/EmployeeService.java
@@ -2,43 +2,56 @@ package com.mr3nz1.entityMappingAndPersistenceLab.services;
 
 import com.mr3nz1.entityMappingAndPersistenceLab.models.Employee;
 import com.mr3nz1.entityMappingAndPersistenceLab.repositories.EmployeeRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class EmployeeService {
     private EmployeeRepository employeeRepository;
+    private TransactionManager transactionManager;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, TransactionManager transactionManager) {
         this.employeeRepository = employeeRepository;
+        this.transactionManager = transactionManager;
     }
 
+    @Cacheable("employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
+    @Cacheable(value = "employee", key = "#id")
     public Employee getEmployeeById(int id) {
         Optional<Employee> employee = employeeRepository.findById(id);
         if (employee.isPresent()) {
-            System.out.println("#####################^^^^^^^^^^^^^^^^^^##################");
-            System.out.println(employee);
-            System.out.println("#####################^^^^^^^^^^^^^^^^^^##################");
             return employeeRepository.findById(id).get();
         }
 
         return null;
     }
 
+
+    @CachePut(value = "employee", key = "#employee.employee_no")
     public Employee addEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    @CachePut(value = "employee", key = "#employee.employee_no")
     public void updateEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
 
+    @CacheEvict(value = "employee", key = "#id")
     public boolean deleteEmployee(int id) {
         try {
             employeeRepository.deleteById(id);
@@ -48,7 +61,20 @@ public class EmployeeService {
         }
     }
 
+    @Cacheable(value = "employee", key = "#firstName")
     public Employee findByFirstName(String firstName) {
         return employeeRepository.findByFirstName(firstName);
+    }
+
+    @Transactional
+    public boolean deleteEmployees(int[] ids) {
+        try {
+            DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+            Arrays.stream(ids).forEach(this::deleteEmployee);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
     }
 }
